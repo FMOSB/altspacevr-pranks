124bcac2161d5df77a6f83eedfb08588f52863c7
Andre Muis <Andre@AndreMuis.com>
2019-05-17 15:07:34 -0700

    Update fart.ts

--- src/fart.ts
+++ src/fart.ts
@@ -50,7 +50,7 @@ export class Fart {
                     local: {
                         position: { x: 0, y: 0.05, z: -0.3 },
                         rotation: MRESDK.Quaternion.RotationAxis(MRESDK.Vector3.Right(), -90 * MRESDK.DegreesToRadians),
-                        scale: { x: 0.45, y: 0.45, z: 0.45 }
+                        scale: { x: 0.5, y: 0.5, z: 0.5 }
                     }
                 },
                 attachment: {
