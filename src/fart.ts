f1f108bc682f55d136166de058655c360c2c2ec8
Andre Muis <Andre@AndreMuis.com>
2019-05-17 15:16:41 -0700

    Update fart.ts

--- src/fart.ts
+++ src/fart.ts
@@ -48,8 +48,8 @@ export class Fart {
             actor: {
                 transform: {
                     local: {
-                        position: { x: 0, y: 0.05, z: -0.35 },
-                        rotation: MRESDK.Quaternion.RotationAxis(MRESDK.Vector3.Right(), -90 * MRESDK.DegreesToRadians),
+                        position: { x: 0, y: 0.05, z: -0.2 },
+                        rotation: MRESDK.Quaternion.RotationAxis(MRESDK.Vector3.Right(), -45 * MRESDK.DegreesToRadians),
                         scale: { x: 0.5, y: 0.5, z: 0.5 }
                     }
                 },
