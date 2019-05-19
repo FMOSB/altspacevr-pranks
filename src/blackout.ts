cac37c7efdf6f9f7234c31aff8f4db8ccb5f12ec
Andre Muis <Andre@AndreMuis.com>
2019-05-19 08:14:48 -0700

    Update blackout.ts

--- src/blackout.ts
+++ src/blackout.ts
@@ -3,7 +3,7 @@ import * as MRESDK from '@microsoft/mixed-reality-extension-sdk'
 import { User } from './common'
 
 export class Blackout {
-    static readonly outwardFacingSphereResourceId = "artifact: 1210908854276063737"
+    static readonly outwardFacingSphereResourceId = "artifact: 1210908859317617147"
     static readonly inwardFacingSphereResourceId = "artifact: 1210908854276063737"
     static readonly durationInMilliseconds = 5000 
 
