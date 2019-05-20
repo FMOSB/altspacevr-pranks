75c0d47e9adb518cffc5c9fb3e675e537d3e5dc7
FMOSB <49654958+FMOSB@users.noreply.github.com>
2019-05-20 15:05:40 +0100

    Update blackout.ts

--- src/blackout.ts
+++ src/blackout.ts
@@ -36,7 +36,7 @@ export class Blackout {
             actor: {
                 transform: {
                     local: {
-                        position: { x: -0.25, y: 0.075, z: 1 },
+                        position: { x: -0.25, y: 0.075, z: 1.5 },
                         scale: { x: 100, y: 100, z: 100 }
                     }
                 },
